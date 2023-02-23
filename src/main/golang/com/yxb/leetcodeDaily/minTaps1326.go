8d69c755d9913c2f91210d2648b4f1e953218f51
yxb <1073086587@qq.com>
2023-02-24 00:51:51 +0800

    困难题，自己做出来的，思路是对的，细节有丶恼火，还是要多画图看，最后两个deque比一个deque还快了1ms

---
+++ src/main/golang/com/yxb/leetcodeDaily/minTaps1326.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+//go要自己手写栈吗
+//func minTaps(n int, ranges []int) int {
+//	currentStart := 0
+//	currentEnd := 0
+//	for i := 0; i < len(ranges); i++ {
+//		tempStart := i - ranges[i]
+//		tempEnd := i + ranges[i]
+//		if tempStart < currentEnd {
+//			if tempEnd > currentEnd {
+//				currentEnd = tempEnd
+//			}
+//		}
+//	}
+//	if {
+//
+//	}
+//}
