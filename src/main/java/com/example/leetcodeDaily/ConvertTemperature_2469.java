bfd4107b3e5a747d16a060ca7f898a85f109f013
yxb <yxb.q@qq.com>
2023-03-21 15:35:54 +0800

    直接进行一个自信的涨

---
+++ src/main/java/com/example/leetcodeDaily/ConvertTemperature_2469.java
@@ -0,0 +1,7 @@
+package com.example.leetcodeDaily;
+
+public class ConvertTemperature_2469 {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius + 273.15, celsius * 1.80 + 32};
+    }
+}
